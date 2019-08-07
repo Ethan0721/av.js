0f13bf72b29137831ebcb5daea311ec1691de7d9
JianZhao <Ethan072158@gmail.com>
2019-08-06 22:16:45 -0400

    update

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ console.log(__filename);
 const MongoClient = require('mongodb').MongoClient;
 var mongodb;
 
-const uri = "mongodb+srv://Ethan_Zhao:Ethan1026@cluster0-xwhjn.mongodb.net/test?retryWrites=true&w=majority";
-
+// const uri = "mongodb+srv://Ethan_Zhao:Ethan1026@cluster0-xwhjn.mongodb.net/test?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI;
 //const url = "mongodb://localhost:27017/cig"
 MongoClient.connect(uri,{
     poolSize:10,
@@ -15,7 +15,7 @@ MongoClient.connect(uri,{
         return;
     }
     console.log(" Connecting Successfully");
-    mongodb = db.db("cig");
+    mongodb = db.db("heroku_g9kr78s9");
     // db.close();
 });
 
